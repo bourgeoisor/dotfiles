2677ec3265e2074f481607fb892676d1da4e36ba
finiks <bourgeoisor@gmail.com>
2013-04-18 00:30:37 -0400

    Added a wm signal for urgent clients

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -438,4 +438,5 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+--client.connect_signal("property::urgent", function() os.execute("mplayer /home/finiks/temp/beep.wav &") end)
 -- }}}
