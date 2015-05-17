d92a8ed81bf90eeff9d8b3dc0128c6e10c3fde53
Olivier Bourgeois <bourgeoisor@gmail.com>
2015-05-17 17:24:02 -0300

    Replaced the makefile with a shell script

--- Makefile
+++
@@ -1,5 +0,0 @@
-all: link
-
-link: config compton.conf irssi mpdconf ncmpcpp vim zsh conkyrc vimrc rtorrent.rc Xdefaults xinitrc zshrc
-	$(foreach file, $^, rm -Rf ~/.$(file) && ln -sf $(CURDIR)/$(file) ~/.$(file);)
-
---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+echo -e "\033[0;34m**********************************\033[0m"
+echo -e "\033[0;34m*** Finiks' dotfiles installer ***\033[0m"
+echo -e "\033[0;34m**********************************\033[0m"
+echo
+echo -e "\033[0;33m    |\033[0m You are about to overwrite every configuration files that might interfere with mine."
+echo -e "\033[0;33m/!\\ |\033[0m Make sure that you have already made the necessary backups."
+while true; do
+    echo -ne "\033[0;33m    |\033[0m "
+    read -p "Do you wish to continue? [Y/n] " INPUT
+    case $INPUT in
+        [Yy]*)
+            break
+            ;;
+        [Nn]*)
+            exit
+            ;;
+    esac
+done
+
+echo
+echo "Cloning oh-my-zsh..."
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+echo
+echo "Cloning zsh-syntax-highlighting..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins
+
+echo
+ITEMS="config compton.conf irssi mpdconf ncmpcpp rtorrent.rc vim vimrc Xdefaults xinitrc zshrc"
+for ITEM in $ITEMS; do
+    echo "Removing any .$ITEM that might exist..."
+    rm -Rf ~/.$ITEM
+    echo "Linking .$ITEM to the home directory..."
+    ln -sf ~/dotfiles/$ITEM ~/.$ITEM
+    echo
+done
+
+echo -e "\033[0;32mAll done!\033[0m"
